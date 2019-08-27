2520e3c9446d2f0bd446c77aff9d015b998b3016
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 02:34:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,5 +11,9 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  min = Integer.max 
+  for element in array{
+    min = element if element < min
+  }
+  min
 end
